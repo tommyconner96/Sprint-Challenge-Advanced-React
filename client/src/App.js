961f9a8ebf0dbe16ab68cc82347f53d5b0785367
unknown <tommyconner96@gmail.com>
2020-04-17 19:36:37 -0400

    data displays

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PlayerCard from './components/PlayerCard'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edist <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <h1>Womens World Cup Players List!</h1>
+      </div>
+      <div className='playercard-container'>
+        <PlayerCard />
+      </div>
     </div>
   );
 }
